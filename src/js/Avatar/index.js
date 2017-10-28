ca6d8fe0fc97c2158a5238f7c4ade5ceeffa16a3
Bartosz Legięć <bartosz@legiec.eu>
2017-10-28 22:45:51 +0200

    Fuck

--- src/js/Avatar/index.js
+++ src/js/Avatar/index.js
@@ -1,6 +1,10 @@
 import Preact from 'preact';
 import { Link } from 'react-router-dom';
 
+import avatarUrl from '../../img/avatar.jpg';
+
+import './avatar.sass';
+
 export default class User extends Preact.Component {
   renderAvatar() {
     if ( !this.props.avatar ) {
@@ -8,7 +12,10 @@ export default class User extends Preact.Component {
     }
 
     return (
-      <img alt={ `${ this.props.fullname || 'avatar' }` } src={ this.props.avatar } />
+      <div className="avatar__image-cont">
+        {/* <img alt={ `${ this.props.fullname || 'avatar' }` } src={ this.props.avatar } /> */}
+        <img alt={ `${ this.props.fullname || 'avatar' }` } src={ avatarUrl } className="avatar__image" />
+      </div>
     );
   }
 
@@ -17,14 +24,14 @@ export default class User extends Preact.Component {
       return null;
     }
     return (
-      <p>{this.props.fullname}</p>
+      <span className="avatar__text">{this.props.fullname}</span>
     );
   }
 
   render() {
     const username = this.props.username;
     return (
-      <Link to={ `user/${ username }` } >
+      <Link to={ `user/${ username }` } className="avatar">
         { this.renderAvatar() }
         { this.renderFullname() }
       </Link>
--- src/js/FeedPage/Post.js
+++ src/js/FeedPage/Post.js
@@ -1,31 +1,28 @@
 import Preact from 'preact';
 import Video from '../Video';
-import User from '../User';
+import Avatar from '../Avatar';
+import Comment from './Comment/';
 import './post.sass';
 
 export default class Post extends Preact.Component {
   renderComments() {
-    return this.props.data.comments.map( ( { user, text, id } ) => (
-      <li key={ id }>
-        <p><User username={ user } fullname={ user } /> { text }</p>
-      </li>
-      ) );
+    return this.props.data.comments.map(
+      ( { user, text, id } ) => ( <Comment key={ id } user={ user } text={ text } /> )
+    );
   }
   render() {
     const { user, video } = this.props.data;
     return (
       <article className="post">
         <header className="post__header">
-          <User avatar={ user } fullname={ user } username={ user } />
+          <Avatar avatar={ user } fullname={ user } username={ user } />
         </header>
         <main>
           <section>
             <Video url={ video } />
           </section>
-          <section>
-            <ul className="comments">
-              { this.renderComments() }
-            </ul>
+          <section className="comments">
+            { this.renderComments() }
           </section>
         </main>
         <input />
