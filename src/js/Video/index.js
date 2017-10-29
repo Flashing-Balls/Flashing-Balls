7c9f41a637038634c6ce5d70c6e106a70f60805c
Bartosz Legięć <bartosz@legiec.eu>
2017-10-29 06:58:53 +0100

    stuff 4

--- src/js/Video/index.js
+++ src/js/Video/index.js
@@ -2,5 +2,12 @@ import Preact from 'preact';
 import './video.sass';
 
 export default ( { url } ) => (
-  <div className="video" style={ { backgroundImage: `url(${ url })` } } />
+  <div className="video">
+    <iframe
+      className="video__iframe"
+      src={ `https://www.youtube.com/embed/${ url }` }
+      frameBorder="0"
+      allowFullScreen
+    />
+  </div>
 );
